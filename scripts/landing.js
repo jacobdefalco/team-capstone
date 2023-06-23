2cf56d414f3755f366c39e7e368cee9dc7021695
Jacob DeFalco <jake.defalco97@gmail.com>
2023-06-23 15:23:56 -0400

    login functionality working

--- scripts/landing.js
+++
@@ -1,2 +0,0 @@
-//This is the scripts for the first page the user sees
-//including presenting the user with the login/register form
--- scripts/login.js
+++ scripts/login.js
@@ -2,3 +2,26 @@
 
 //Write some code AND IF the login/password matches up with data in the /api/users then it is a successful login
 //Give token, courtesy of the scripts within auth.js
+
+"use strict";
+
+const loginForm = document.querySelector("#login");
+
+loginForm.onsubmit = function (event) {
+    // Prevent the form from refreshing the page,
+    // as it will do by default when the Submit event is triggered:
+    event.preventDefault();
+
+    // We can use loginForm.username (for example) to access
+    // the input element in the form which has the ID of "username".
+    const loginData = {
+        username: loginForm.username.value,
+        password: loginForm.password.value,
+    }
+
+    // Disables the button after the form has been submitted already:
+    loginForm.loginButton.disabled = true;
+
+    // Time to actually process the login using the function from auth.js!
+    login(loginData);
+};
