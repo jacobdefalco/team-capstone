eabab70eebe947a7ae8948ce9e122b7b86d86802
Jacob DeFalco <jake.defalco97@gmail.com>
2023-06-26 15:32:13 -0400

    final commit 6-26

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const usernameInput = document.querySelector("#username-input")
+const passwordInput = document.querySelector("#password-input")
+const rememberMeCheckbox = document.querySelector("#remember-me-checkbox")
+const loginButton = document.querySelector("#loginButton")
+const registerButton = document.querySelector("#register-button")
+
