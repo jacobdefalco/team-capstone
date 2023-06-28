a41c20d130d043b91da1e52bb77a5706a6650f9e
Jacob DeFalco <jake.defalco97@gmail.com>
2023-06-28 13:58:05 -0400

    updated scripts

--- scripts/global.js
+++ scripts/global.js
@@ -1,9 +1,12 @@
 "use strict";
 
 const userGreeting = document.querySelector("#user-greeting-div");
+const logoutButton = document.querySelector("#logout-btn");
 
 const loginData = getLoginData();
 
 if (isLoggedIn()) {
   userGreeting.innerHTML = `Logged in as ${loginData.username}`;
+} else {
+  userGreeting.innerHTML = "Please Log In";
 }
